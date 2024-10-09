b7a0d73788385d1c5c893509c7fc858b3a53beb3
Koushik6692 <105788507+Koushik6692@users.noreply.github.com>
2024-10-09 11:50:50 +0530

    Api response and error Handling(Utils code)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`SERVER RUNNING AT PORT ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log(`MONGODB CONNECTION FAILED!!${err}`);
+  });
---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    stausCode,
+    message = "Something Went Wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.status = statusCode;
+    this.message = message;
+    this.data = null;
+    this.errors = errors;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
