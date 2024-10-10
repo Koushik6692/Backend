bab9591b5df26a80754610a4fe0fe976c1601d4a
Koushik6692 <105788507+Koushik6692@users.noreply.github.com>
2024-10-10 14:56:17 +0530

    production lvl setup completed(defined routes connection)

--- src/app.js
+++ src/app.js
@@ -13,7 +13,12 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.route.js";
+
+//routes declartion
+app.use("/api/v1/users", userRouter);
 
 export default app;
